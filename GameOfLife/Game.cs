72fb1e47468ae07397178cd760b79d57073b6ea7
BloodyiceTea <rico.w.wolf@t-online.de>
2021-04-19 12:01:47 +0200

    Kleine Anpassung, Rechtschreibfehler

---
+++ GameOfLife/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Game
+    {
+        private int Gen { get; set; }
+        private int CellCount { get; set; }
+        private int Height { get; set; }
+        private int Width { get; set; }
+        //private cell[] {get;set;}
+        private int Interval { get; set; }
+        private bool TorusRules { get; set; }
+
+
+        public void CheckCell()
+        {
+            if (!TorusRules)
+            {
+
+            }
+            
+        }
+
+        public void CheckCellTorus()
+        {
+            if (TorusRules)
+            {
+
+            }
+
+        }
+
+        public void NextGen()
+        {
+            this.Gen += 1;
+
+        }
+
+    }
+}
