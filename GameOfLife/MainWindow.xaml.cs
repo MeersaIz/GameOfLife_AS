21fc8d73fdea14fc72853f5da9838fd84cf8e41a
BloodyiceTea <rico.w.wolf@t-online.de>
2021-04-19 16:27:46 +0200

    Abfrage Anzahl Benachbarte Zellen am Leben erstellt

--- GameOfLife/MainWindow.xaml.cs
+++ GameOfLife/MainWindow.xaml.cs
@@ -25,9 +25,10 @@ namespace GameOfLife
 
     public partial class MainWindow : Window
     {
-        //Global Cons
+        //Global Cons/ Vars
         const int width = 16;
         const int height = 9;
+        Rectangle[,] Cells = new Rectangle[width, height];
 
         public MainWindow()
         {
@@ -36,6 +37,9 @@ namespace GameOfLife
 
         private void btn_create_Click(object sender, RoutedEventArgs e)
         {
+            //Töte Kinder
+            can_gamefield.Children.Clear();
+
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
@@ -56,6 +60,9 @@ namespace GameOfLife
                         //Position der Zellen bestimmen/ setzten
                         Canvas.SetLeft(Cell, x * can_gamefield.ActualWidth / width);
                         Canvas.SetTop(Cell, y * can_gamefield.ActualHeight / height);
+
+                    //Positions-Bennung der Zellen
+                    Cells[x, y] = Cell;
 
                     //Klickfunktion Zelle (tod/lebend) erstellen
                     Cell.MouseDown += Cell_MouseDown;
@@ -83,12 +90,66 @@ namespace GameOfLife
         //Geneartionssprung via Button_Click
         private void btn_nextGen_Click(object sender, RoutedEventArgs e)
         {
+            int[,] num_CellNeighbors = new int[height, width];
+
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    
+                    int CountNeighbors_alive = 0;
 
+                    //Check Rand des Feldes + Setzten auf andere Seite des Feldes
+                    int x_up = x - 1;
+                    if(x_up < 0)
+                    { x_up = width - 1; }
+
+                    int x_down = x + 1;
+                    if(x_down >= width)
+                    { x_down = 0; }
+
+                    int y_up = y - 1;
+                    if (y_up < 0)
+                    { y_up = height - 1; }
+
+                    int y_down = y + 1;
+                    if (y_down >= height)
+                    { y_down = 0; }
+
+
+                    //Nachbar Links Oben
+                    if (Cells[x_up, y_up].Fill == Brushes.Green)
+                    { CountNeighbors_alive++; }
+
+                    //Nachbar Mitte Oben
+                    if (Cells[x, y_up].Fill == Brushes.Green)
+                    { CountNeighbors_alive++; }
+
+                    //Nachbar Rechts Oben
+                    if (Cells[x_down, y_up].Fill == Brushes.Green)
+                    { CountNeighbors_alive++; }
+
+                    //Nachbar Links Mitte
+                    if (Cells[x_up, y].Fill == Brushes.Green)
+                    { CountNeighbors_alive++; }
+
+                    //Nachbar Rechts Mitte
+                    if (Cells[x_down, y].Fill == Brushes.Green)
+                    { CountNeighbors_alive++; }
+
+                    //Nachbar Links Unten
+                    if (Cells[x_up, y_down].Fill == Brushes.Green)
+                    { CountNeighbors_alive++; }
+
+                    //Nachbar Mitte Unten
+                    if (Cells[x, y_down].Fill == Brushes.Green)
+                    { CountNeighbors_alive++; }
+
+                    //Nachbar Rechts Unten
+                    if (Cells[x_down, y_down].Fill == Brushes.Green)
+                    { CountNeighbors_alive++; }
+
+                    //Speichern der Lebenden Nachbarn pro Zelle
+                    num_CellNeighbors[y, x] = CountNeighbors_alive;
                 }
             }
 
