679b9fd17da4496f10c2b5fc89c80777ba4a9bb1
Daniel Wesierski <wesierski_daniel@oszimt.onmicrosoft.com>
2021-04-19 11:36:34 +0200

    Add Cell class

---
+++ GameOfLife/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Cell
+    {
+        private bool status;
+        private int posX;
+        private int posY;
+
+        public Cell(int posX, int posY)
+        {
+            this.status = false;
+            this.posX = posX;
+            this.posY = posY;
+        }
+
+        public static bool GetStatus()
+        {
+            return this.status;
+        }
+
+        public static void SetStatus(bool status)
+        {
+            this.status = status;
+        }
+
+        public static int GetPosY()
+        {
+            return this.posY;
+        }
+        public static int GetposX()
+        {
+            return this.posX;
+        }
+
+
+    }
+}
